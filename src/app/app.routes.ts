f13c91513e93d04f534b4437f9eaec106df588e4
Mars-was-taken <marcoisanotaku@gmail.com>
2025-07-03 14:41:32 +0200

    switch from homepage to gamepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Router, Routes } from '@angular/router';
-import * as homepageComponent from './pages/homepage/homepage.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { GamepageComponent } from './pages/gamepage/gamepage.component';
 
 import { AppComponent } from './app.component';
-import {ResoursesComponent} from './components/resources/resources.component';
+import { ResoursesComponent } from './components/resources/resources.component';
 
 export const routes: Routes = [
-  { path: '', component: homepageComponent.HomepageComponent }, // Default route
+  { path: '', component: HomepageComponent }, // Default route
+  { path: 'game', component: GamepageComponent },
   { path: '**', redirectTo: '' } // Fallback route
 ];
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -19,16 +20,19 @@ export class HomepageComponent {
   inputNameJoin: any = '';
   inputIDJoin: any = '';
 
+  constructor(private router: Router) {}
 
   startMultiplayer() {
     if (this.sessionName) {
       console.log('Creating multiplayer session:', this.sessionName);
+      this.router.navigate(['/game']);
     }
   }
 
   joinMultiplayer() {
     if (this.playerName) {
       console.log('Joining multiplayer session:', this.playerName);
+      this.router.navigate(['/game']);
     }
   }
 
