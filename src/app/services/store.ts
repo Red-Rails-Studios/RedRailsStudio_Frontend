675b3ed56b667775cc5e243f91137fb7e42bba1f
Mars-was-taken <marcoisanotaku@gmail.com>
2025-10-17 15:03:23 +0200

    .

--- src/app/services/store.ts
+++ src/app/services/store.ts
@@ -37,6 +37,7 @@ export class Store {
     playerInfo = signal<Player>({ 
         uId: '',
         name: '',
+        color: '',
         trains: [] as Train[],
         rails: [] as Rail[],
         stations: [] as Station[],
