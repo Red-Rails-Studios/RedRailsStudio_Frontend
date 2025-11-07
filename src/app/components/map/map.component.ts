d36fa85cc67908f6aa718528e6953385e799f4d4
Mars-was-taken <marcoisanotaku@gmail.com>
2025-11-07 19:49:30 +0100

    .

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -525,7 +525,7 @@ export class MapComponent implements OnInit, AfterViewInit, OnDestroy {
               ctx.beginPath();
               const radius = Math.max(4, Math.floor(Math.min(cellWidth, cellHeight) * 0.18));
               ctx.arc(centerX + Math.floor(Math.random() * 10), centerY + Math.floor(Math.random() * 10), radius, 0, Math.PI * 2);
-              ctx.fillStyle = "#6b6b6bff";
+              ctx.fillStyle = "#999999ff";
               ctx.fill();
               continue;
             }
